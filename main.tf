b5d3a561113c7e6a39fd53a03cf3eff014ed0f38
Your Name <you@example.com>
2022-11-12 17:41:42 +0100

    Provisioned Ubuntu webserver with Internet gateway + routing + firewall setup

--- main.tf
+++ main.tf
@@ -17,3 +17,128 @@ provider "aws" {
 }
 
 
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "prod-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+  tags = {
+    Name = "main-gw"
+  }
+}
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod-route-table"
+  }
+
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+resource "aws_route_table_association" "prod-route-assoc" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = "443"
+    to_port     = "443"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-web"
+  }
+}
+
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+}
+
+resource "aws_eip" "web-server-eip" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [
+    aws_internet_gateway.gw
+  ]
+}
+
+resource "aws_instance" "web-server-inst" {
+  ami               = var.instance_ami
+  instance_type     = var.instance_type
+  availability_zone = var.availability_zone
+  key_name          = "main-key"
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install nginx -y
+                sudo systemctl start nginx
+                EOF
+  tags = {
+    Name = "web-server"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,13 +1,22 @@
 variable "instance_ami" {
+  type        = string
+  description = "Ubuntu 22.04 Free Tier AMI"
   default     = "ami-08c40ec9ead489470"
-  description = "Centos8 Free Tier AMI"
 }
 variable "region" {
-  default     = "us-east-1"
+  type        = string
   description = "East region - default"
+  default     = "us-east-1"
 }
 
 variable "instance_type" {
+  type        = string
+  description = "Type of instance"
   default     = "t2.micro"
-  description = "Description"
+}
+
+variable "availability_zone" {
+  type        = string
+  description = "Availability zone"
+  default     = "us-east-1a"
 }
